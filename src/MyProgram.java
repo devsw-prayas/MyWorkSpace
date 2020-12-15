633c1a3ebfdda910241b5b3e0f8cee0fee3b675a
DevSw <prayas4494@gmail.com>
2020-12-15 03:36:30 +0000

    New Program

---
+++ src/MyProgram.java
@@ -0,0 +1,9 @@
+package src;
+
+public class MyProgram{
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("This is a program");
+    }
+}
