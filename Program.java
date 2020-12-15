e09a60789a9efdb1628b39aabae07114d09112e9
DevSw <prayas4494@gmail.com>
2020-12-15 04:39:06 +0000

    Done

--- Program.java
+++ Program.java
@@ -3,5 +3,6 @@ public class Program{
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("This is a program");
+        System.out.println("Small edit");
     }
 }
